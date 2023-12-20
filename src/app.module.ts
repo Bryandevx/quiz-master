4acdd008be083e086bd84665a9de8655e5aa5eb9
Bryan Solano <bryan99.solano.saborio@gmail.com>
2023-12-20 03:52:02 -0600

    chore(app-module) add entitties for the app module to implement

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
 
 import { ConfigModule } from 'src/shared/config/config.module';
+
 import { UserModule } from './api/user/user.module';
+
+import { TestModule } from './api/test/test.module';
+
+import { QuestionModule } from './api/question/question.module';
+
+import { AnswerModule } from './api/answer/answer.module';
+
 import { PrismaModule } from './shared/datasource/prisma.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule, UserModule],
+  imports: [
+    PrismaModule,
+    ConfigModule,
+    UserModule,
+    TestModule,
+    QuestionModule,
+    AnswerModule,
+  ],
   controllers: [],
   providers: [],
 })
